7936c553fa80a2436a06b0ab86f63fbc6b66b9f4
Jan Havlin <jan@janhavlin.com>
2026-04-20 00:50:59 +0200

    chore: set Astro site URL to janhavlin.com for canonical links and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://havlinj.github.io',
+  site: 'https://janhavlin.com',
   integrations: [sitemap()],
 });
